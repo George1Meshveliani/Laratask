ab3360822af277765090e269751ad6be91731858
George1Meshveliani <giorgi.meshveliani@gmail.com>
2022-06-23 13:11:28 +0400

    implement user controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,8 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UserController extends Controller
 {
 
+    /**
+     * Get all users data
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function allUsers() {
+        $users = User::all();
+        return response()->json([
+            'data' => $users,
+        ]);
+    }
 }
