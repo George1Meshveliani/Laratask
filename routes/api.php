83e319ee0427bef4a75c33c1d23e58a472d31a83
George1Meshveliani <giorgi.meshveliani@gmail.com>
2022-06-23 13:18:42 +0400

    create api endpoint for users

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Endpoint: http://{url}/api/users
+// In our example http://localhost:8000/api/users
+Route::get('users', [UserController::class, 'allUsers']);
